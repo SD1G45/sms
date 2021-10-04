efdc3c589e66f33da0c26dc219456e40e3359c7e
Zachary <rick.jsventures.com>
2021-10-04 12:32:32 -0400

    Test commit

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 const index = () => {
+const [test, setTest] = useState(0);
   return (
     <div>
-        <p>hello</p>
+        <p>Click the button!</p>
+        <button onClick={e =>setTest(test + 1)}>
+            {test}
+        </button>
+
+        <button onClick = {e => setTest(0)}>
+            reset the button!
+        </button>
+
     </div>
   )
 }
