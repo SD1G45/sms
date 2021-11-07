8b2159322797e3911af3bcd130efa92a77a32a12
Walter Galdamez <walter@DESKTOP-HBB309Q.localdomain>
2021-11-07 14:05:36 -0500

    creating newCouponMutation and graphql Coupon object

---
+++ graphql/schema/objects/Coupon.ts
@@ -0,0 +1,13 @@
+import { objectType } from "nexus";
+
+export const Coupon = objectType({
+    name: "Coupon",
+    definition(t) {
+        t.string("name");
+        t.string("title");
+        t.string("description");
+        t.string("primaryColor");
+        t.string("expirationDate");
+        t.string("businessId");
+    },
+});
