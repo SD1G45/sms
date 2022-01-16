269e306957e6f8adeba5e9c611b76e83ce24a41e
Zachary <rick.jsventures.com>
2022-01-16 10:59:35 -0500

    added icons to navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -14,6 +14,7 @@ import {
 } from "./styles";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 const Navbar = () => {
   const router = useRouter();
@@ -33,9 +34,9 @@ const Navbar = () => {
         </BusinessInfoSection>
         <ControlsSection>
           <CreateButton invert>Create</CreateButton>
-          <Icon />
+          <Image src="/icons/settings.svg" width={50} height={50} />
           <Spacing />
-          <Icon />
+          <Image src="/icons/profile.svg" width={70} height={50} />
         </ControlsSection>
       </PrimaryNavbar>
       <SecondaryNavbarContainer>
