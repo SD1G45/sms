06420c3955fa50e9124719f50ef65e6109d42fe5
Walter Galdamez <walter@DESKTOP-HBB309Q.localdomain>
2022-01-17 12:27:49 -0500

    Table component

---
+++ components/Table/styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const DivContainer = styled.div`
+    padding-left: 50px;
+    padding-top: 30px;
+`;
+
+export const DataTable = styled.table`
+    padding-top: 30px;
+    border-spacing: 0px 0px;
+    borderCollapse: collapse; 
+`;
+
+export const TableBody = styled.tbody`
+
+`;
+
+export const Header = styled.th`
+    border-bottom: 2.75px solid #C4C4C4;
+    padding-bottom: 15px;
+    padding-right: 60px;
+    padding-top: 15px;
+    font-size: 14px;
+`;
+
+export const HeaderLong = styled.th`
+    border-bottom: 2.75px solid #C4C4C4;
+    padding-bottom: 15px;
+    padding-right: 100px;
+    padding-top: 15px;
+    font-size: 14px;
+    text-content: left;
+`;
