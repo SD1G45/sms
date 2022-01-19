5f64bf7d95ae8df5338131753aedb0b0f19bc7cb
Denielle Abaquita <denielle.abaquita@gmail.com>
2022-01-19 14:35:19 -0500

    Added create campaign page

--- pages/campaigns/Campaigns.tsx
+++ pages/campaigns/Campaigns.tsx
@@ -8,13 +8,13 @@ import { ColumnDiv, ContainerDiv, RowDiv, SearchDiv, StyledHeader } from "../../
 const Campaigns = () => {
 
   const sideNavItems: string[] = ["Analytics", "Create New", "FAQ"];
-  const routes: string[] = ["/campaigns", "/create-campaigns", "/faq-campaigns"];
+  const routes: string[] = ["/campaigns", "/create-campaign", "/faq-campaign"];
   const tableHeaders: string[] = ["Campaign", "Date sent", "Messages sent", "Message success rate", "Coupons opened", "Coupons redeemed", "Coupon open %", "Coupon redeem %"]; 
   const dummyData: string[] = ["Main List - 10% OFF", "10/20/21", "8,123", "99%", "5,400", "400", "66%", "0.5%"];
 
   return (
     <ContainerDiv>
-      <SideNav items={sideNavItems} routes={routes} heading={"Keywords"}/>
+      <SideNav items={sideNavItems} routes={routes} heading={"Campaigns"}/>
       <ColumnDiv>
           <RowDiv>
             <StyledHeader>Campaign Analytics</StyledHeader>
