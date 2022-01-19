27bfb204c4ac6e268439b0b6ba7003affdf0af61
Denielle Abaquita <denielle.abaquita@gmail.com>
2022-01-19 14:17:29 -0500

    created campaign page

---
+++ pages/campaigns/Campaigns.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Button from "../../components/Button";
+import SearchBar from "../../components/SearchBar";
+import SideNav from "../../components/SideNav";
+import Table from "../../components/Table";
+import { ColumnDiv, ContainerDiv, RowDiv, SearchDiv, StyledHeader } from "../../page-styles/coupons/styles";
+
+const Campaigns = () => {
+
+  const sideNavItems: string[] = ["Analytics", "Create New", "FAQ"];
+  const routes: string[] = ["/campaigns", "/create-campaigns", "/faq-campaigns"];
+  const tableHeaders: string[] = ["Campaign", "Date sent", "Messages sent", "Message success rate", "Coupons opened", "Coupons redeemed", "Coupon open %", "Coupon redeem %"]; 
+  const dummyData: string[] = ["Main List - 10% OFF", "10/20/21", "8,123", "99%", "5,400", "400", "66%", "0.5%"];
+
+  return (
+    <ContainerDiv>
+      <SideNav items={sideNavItems} routes={routes} heading={"Keywords"}/>
+      <ColumnDiv>
+          <RowDiv>
+            <StyledHeader>Campaign Analytics</StyledHeader>
+            <SearchDiv>
+              <SearchBar value={""} onValueChange={() => {}}/>
+            </SearchDiv>
+            <Button>Create New Campaign</Button>
+          </RowDiv>
+          <Table headers={tableHeaders} data={dummyData}/>
+      </ColumnDiv>
+    </ContainerDiv>
+  );
+};
+
+export default Campaigns;
