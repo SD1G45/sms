990fa1be858702aff9ecba257e04281e0aff70a5
Denielle Abaquita <denielle.abaquita@gmail.com>
2022-01-21 10:22:55 -0500

    Added route from button to create keyword

--- pages/keywords/Keywords.tsx
+++ pages/keywords/Keywords.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import React from "react";
 import Button from "../../components/Button";
 import SearchBar from "../../components/SearchBar";
@@ -7,10 +8,18 @@ import { ColumnDiv, ContainerDiv, RowDiv, SearchDiv, StyledHeader } from "../../
 
 const Keywords = () => {
 
+  const router = useRouter();
+  const createPath = router.asPath + "/create";
+  const faqPath = router.asPath + "/faq"
+
   const sideNavItems: string[] = ["Analytics", "Create New", "FAQ"];
-  const routes: string[] = ["/keywords", "/keywords/create", "/keywords/faq"];
+  const routes: string[] = ["/keywords", createPath, faqPath];
   const tableHeaders: string[] = ["Keyword", "Date created", "Messages sent", "Message success rate", "Coupons opened", "Coupons redeemed", "Coupon open %", "Coupon redeem %"]; 
   const dummyData: string[] = ["5OFF", "10/20/21", "8,123", "99%", "5,400", "400", "66%", "0.5%"];
+
+  const onClick = () => {
+    router.push(createPath);
+  };
 
   return (
     <ContainerDiv>
@@ -21,7 +30,7 @@ const Keywords = () => {
             <SearchDiv>
               <SearchBar value={""} onValueChange={() => {}}/>
             </SearchDiv>
-            <Button>Create New Keyword</Button>
+            <Button onClick={onClick}>Create New Keyword</Button>
           </RowDiv>
           <Table headers={tableHeaders} data={dummyData}/>
       </ColumnDiv>
