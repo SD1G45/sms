dcdb0fe867a1d92d07adcc20dae49ae8e66ea2a8
Denielle Abaquita <denielle.abaquita@gmail.com>
2022-01-21 09:55:34 -0500

    Added route to new create page for campaign

--- pages/campaigns/Campaigns.tsx
+++ pages/campaigns/Campaigns.tsx
@@ -3,14 +3,21 @@ import Button from "../../components/Button";
 import SearchBar from "../../components/SearchBar";
 import SideNav from "../../components/SideNav";
 import Table from "../../components/Table";
+import { useRouter } from "next/router";
 import { ColumnDiv, ContainerDiv, RowDiv, SearchDiv, StyledHeader } from "../../page-styles/coupons/styles";
 
 const Campaigns = () => {
 
+  const router = useRouter();
+
   const sideNavItems: string[] = ["Analytics", "Create New", "FAQ"];
-  const routes: string[] = ["/campaigns", "/create-campaign", "/faq-campaign"];
+  const routes: string[] = ["/campaigns", router.asPath + "/create", "/faq-campaign"];
   const tableHeaders: string[] = ["Campaign", "Date sent", "Messages sent", "Message success rate", "Coupons opened", "Coupons redeemed", "Coupon open %", "Coupon redeem %"]; 
   const dummyData: string[] = ["Main List - 10% OFF", "10/20/21", "8,123", "99%", "5,400", "400", "66%", "0.5%"];
+
+  const onClick = () => {
+    router.push(router.asPath + "/create");
+  };
 
   return (
     <ContainerDiv>
@@ -21,7 +28,7 @@ const Campaigns = () => {
             <SearchDiv>
               <SearchBar value={""} onValueChange={() => {}}/>
             </SearchDiv>
-            <Button>Create New Campaign</Button>
+            <Button onClick={onClick}>Create New Campaign</Button>
           </RowDiv>
           <Table headers={tableHeaders} data={dummyData}/>
       </ColumnDiv>
