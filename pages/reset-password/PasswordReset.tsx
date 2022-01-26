49e5d6eb12673f0f76b3fd27b159ca43ffa04b4f
Zach Mayers <zachmayers@MacBook-Pro.local>
2022-01-26 10:35:33 -0500

    Reset Password frontend

--- pages/reset-password/PasswordReset.tsx
+++ pages/reset-password/PasswordReset.tsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from "react";
 import Card from "../../components/Card";
 import SingleCardPage from "../../components/SingleCardPage";
 import {
+  BackButton,
+  HeaderDiv,
   NewPasswordInput,
+  StyledButton,
   StyledCard,
   StyledHeader,
 } from "../../page-styles/password-reset/styles";
@@ -12,12 +15,15 @@ import ErrorPopup from "../../components/ErrorPopup";
 import { useMutation } from "@apollo/client";
 import { RESET_PASSWORD_MUTATION } from "../../page-mutations/reset-password";
 import { resetPasswordMutation } from "../../graphql/schema/mutations";
+
 const PasswordReset = () => {
   const [oldPassword, setOldPassword] = useState("");
   const [confirmOldPassword, setConfirmOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [errorState, setErrorState] = useState({ error: false, message: "" });
-
+  const [updatePassword, setUpdatePassword] = useState(false);
+  const [forgotPassword, setForgotPassword] = useState(false);
+  const [email, setEmail] = useState("");
   const [updatePasswordMutation] = useMutation(RESET_PASSWORD_MUTATION, {
     errorPolicy: "all",
   });
@@ -76,37 +82,91 @@ const PasswordReset = () => {
     }
   };
 
+  const InitialDisplay = () => {
+    return (
+      <>
+        <StyledCard>
+          <HeaderDiv>
+            <StyledHeader>Reset Password</StyledHeader>
+          </HeaderDiv>
+          <Button onClick={() => setUpdatePassword(true)}>
+            I know my password
+          </Button>
+          <Button onClick={() => setForgotPassword(true)}>
+            I forgot my password
+          </Button>
+        </StyledCard>
+      </>
+    );
+  };
+
+  // TODO: Write check for existing Email as well as reset pass mutation
+  const ForgotPasswordDisplay = () => {
+    return (
+      <>
+        <StyledCard>
+          <HeaderDiv>
+            {" "}
+            <StyledHeader>Reset Password</StyledHeader>
+          </HeaderDiv>
+          <TextField
+            label="Enter the email associated with your password"
+            value={email}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(event.target.value)
+            }
+          />
+          <StyledButton>Send Email</StyledButton>
+          <BackButton onClick={() => setForgotPassword(false)}>Back</BackButton>
+        </StyledCard>
+      </>
+    );
+  };
+
+  const UpdatePasswordDisplay = () => {
+    return (
+      <>
+        <StyledCard>
+          <HeaderDiv>
+            <StyledHeader>Reset Password</StyledHeader>
+          </HeaderDiv>
+          <TextField
+            label="Enter your old password"
+            value={oldPassword}
+            type="password"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setOldPassword(event.target.value)
+            }
+          />
+          <TextField
+            label="Re-enter your old password"
+            value={confirmOldPassword}
+            type="password"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setConfirmOldPassword(event.target.value)
+            }
+          />
+          <ErrorPopup error={errorState.error} message={errorState.message} />
+          <NewPasswordInput
+            label="Enter your New password"
+            value={newPassword}
+            type="password"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setNewPassword(event.target.value)
+            }
+          />
+          <Button onClick={() => onReset()}>Reset Password</Button>
+          <BackButton onClick={() => setUpdatePassword(false)}>back</BackButton>
+        </StyledCard>
+      </>
+    );
+  };
+
   return (
     <SingleCardPage>
-      <StyledCard>
-        <StyledHeader>Reset Password</StyledHeader>
-        <TextField
-          label="Enter your old password"
-          value={oldPassword}
-          type="password"
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setOldPassword(event.target.value)
-          }
-        />
-        <TextField
-          label="Re-enter your old password"
-          value={confirmOldPassword}
-          type="password"
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setConfirmOldPassword(event.target.value)
-          }
-        />
-        <ErrorPopup error={errorState.error} message={errorState.message} />
-        <NewPasswordInput
-          label="Enter your New password"
-          value={newPassword}
-          type="password"
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setNewPassword(event.target.value)
-          }
-        />
-        <Button onClick={() => onReset()}>Reset Password</Button>
-      </StyledCard>
+      {!updatePassword && !forgotPassword && <InitialDisplay />}
+      {updatePassword && !forgotPassword && <UpdatePasswordDisplay />}
+      {!updatePassword && forgotPassword && <ForgotPasswordDisplay />}
     </SingleCardPage>
   );
 };
