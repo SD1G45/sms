f70fcff894910d1f22fe244f65d1094ecbf6c5fe
Zach Mayers <zachmayers@MacBook-Pro.local>
2022-01-31 14:26:46 -0500

    Removed styles.ts from pages/campaigns, moved import to page-styles/camapigns

--- pages/campaigns/Campaigns.tsx
+++ pages/campaigns/Campaigns.tsx
@@ -11,7 +11,7 @@ import {
   ContainerDiv,
   StyledHeader,
 } from "../../page-styles/coupons/styles";
-import { CampaignHeader } from "./styles";
+
 import { HeaderDiv } from "../../page-styles/campaigns/styles";
 
 const Campaigns = () => {
@@ -31,16 +31,18 @@ const Campaigns = () => {
     "Coupon open %",
     "Coupon redeem %",
   ];
-  const dummyData: string[][] = [[
-    "Main List - 10% OFF",
-    "10/20/21",
-    "8,123",
-    "99%",
-    "5,400",
-    "400",
-    "66%",
-    "0.5%",
-  ]];
+  const dummyData: string[][] = [
+    [
+      "Main List - 10% OFF",
+      "10/20/21",
+      "8,123",
+      "99%",
+      "5,400",
+      "400",
+      "66%",
+      "0.5%",
+    ],
+  ];
 
   const onClick = () => {
     router.push(createPath);
