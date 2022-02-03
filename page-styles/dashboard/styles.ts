0bcbd15948526aca2b9042ef4bbd212df984f060
dabaquita <denielle.abaquita@gmail.com>
2022-02-03 12:30:53 -0500

    Finished styling analytics row

--- page-styles/dashboard/styles.ts
+++ page-styles/dashboard/styles.ts
@@ -15,6 +15,27 @@ export const ContainerDiv = styled.div`
   display: flex;
   white-space: nowrap;
   justify-content: center;
+  padding: 15px 80px;
+`;
+
+export const BorderDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  border: 1px solid #BEC8D2;
+  border-style: solid none;
+`;
+
+export const LeftChartDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 21px 50px 21px 0px;
+  justify-content: center;
+  width: 100%
+`;
+
+export const RightChartDiv = styled(LeftChartDiv)`
+  padding: 21px 50px 21px 50px;
 `;
 
 export const ColumnDiv = styled.div`
@@ -23,6 +44,17 @@ export const ColumnDiv = styled.div`
   padding: 50px;
   width: 100%;
   align-items: center;
+`;
+
+export const SalesColumnDiv = styled(ColumnDiv)`
+  border-left: 1px solid #BEC8D2;
+  justify-content: center;
+  padding: 0px;
+`;
+
+export const SalesInnerColumnDiv = styled(ColumnDiv)`
+  padding: 30px 19px 30px 19px;
+  gap: 11px;
 `;
 
 export const RowDiv = styled.div`
@@ -34,8 +66,44 @@ export const RowDiv = styled.div`
   margin-right: 10%;
 `;
 
+export const SalesRowDiv = styled(RowDiv)`
+  padding: 0px;
+`;
+
+export const SalesInnerRowTitleDiv = styled(RowDiv)`
+  padding: 0px;
+  gap: 0%;
+  margin: 0%;
+`;
+
 export const StyledHeader = styled.h1`
-  padding-left: 50px;
-  padding-right: 50px;
-  text-align: center;
+  text-align: left;
+  margin-top: 30px;
+  width: 100%;
+`;
+
+export const SalesSmallText = styled.text`
+  text-align: left;
+  width: 100%;
+  color: #888B8E;
+  font-size: 12px;
+`;
+
+export const SalesLargeText = styled.text`
+  text-align: left;
+  width: 100%;
+  font-size: 30px;
+`;
+
+export const SalesLinkText = styled.text`
+  text-align: right;
+  width: 100%;
+  color: #4881F0;
+  font-size: 8;
+`;
+
+export const SalesDivider = styled.div`
+  width: 100%; 
+  border-bottom: 1px solid #BEC8D2;
+  margin: 15px 0px 7px 0px;
 `;
---
+++ pages/dashboard/Dashboard.tsx
@@ -0,0 +1,152 @@
+import React, { useEffect, useState } from "react";
+import Card from "../../components/Card";
+import SingleCardPage from "../../components/SingleCardPage";
+import Image from "next/image";
+
+import {
+  ContainerDiv,
+  ColumnDiv,
+  RowDiv,
+  StyledCard,
+  BorderDiv,
+  LeftChartDiv,
+  RightChartDiv,
+  StyledHeader,
+  SalesColumnDiv,
+  SalesRowDiv,
+  SalesInnerColumnDiv,
+  SalesInnerRowTitleDiv,
+  SalesSmallText,
+  SalesLargeText,
+  SalesLinkText,
+  SalesDivider
+} from "../../page-styles/dashboard/styles";
+import Link from "next/link";
+import { useBusinessState } from "../../context/BusinessContext/BusinessContext";
+import dynamic from "next/dynamic";
+import sampleData from "../../sampleData/sampleData";
+
+const LineChart = dynamic(() => import("../../components/LineChart"), {
+  ssr: false,
+});
+
+const couponData = sampleData.couponData();
+const customersData = sampleData.customersData();
+
+const Dashboard = () => {
+  const businessState = useBusinessState();
+  const [businessName, setBusinessName] = useState("");
+  useEffect(() => {
+    setBusinessName(businessState?.name || "");
+    console.log(businessName);
+  });
+
+  return (
+    <>
+      <ContainerDiv>
+        <StyledHeader>Today</StyledHeader>
+      </ContainerDiv>
+
+      <ContainerDiv>
+        <BorderDiv>
+          <LeftChartDiv> 
+            <LineChart
+              title="Coupons"
+              data={couponData}
+              height={300}
+              flexure={1}
+            />
+          </LeftChartDiv>
+          <RightChartDiv>
+            <LineChart
+              title="Customers"
+              data={customersData}
+              height={300}
+              flexure={1}
+            />
+          </RightChartDiv>
+          <SalesColumnDiv>
+            <SalesRowDiv>
+              <SalesInnerColumnDiv>
+                <SalesInnerRowTitleDiv>
+                  <SalesSmallText>Amount Spent</SalesSmallText>
+                  <SalesLinkText>View current billing cycle</SalesLinkText>
+                </SalesInnerRowTitleDiv>
+                <SalesLargeText>$1,180.00</SalesLargeText>
+                <SalesSmallText>Billing cycle: 10/07/21 - 10/31/21</SalesSmallText>
+              </SalesInnerColumnDiv>
+            </SalesRowDiv>
+            <SalesDivider/>
+            <SalesRowDiv>
+              <SalesInnerColumnDiv>
+                <SalesInnerRowTitleDiv>
+                  <SalesSmallText>Estimated sales</SalesSmallText>
+                  <SalesLinkText>View billing cycle</SalesLinkText>
+                </SalesInnerRowTitleDiv>
+                <SalesLargeText>$175.00</SalesLargeText>
+                <SalesSmallText>*Estimation only*</SalesSmallText>
+              </SalesInnerColumnDiv>
+            </SalesRowDiv>
+          </SalesColumnDiv>
+        </BorderDiv>
+      </ContainerDiv>
+
+      <ContainerDiv>
+        <ColumnDiv>
+          <RowDiv>
+            <StyledCard>
+              <StyledHeader>Campaigns</StyledHeader>
+              <Link href="/campaigns">
+                <Image
+                  src="/Dashboard_icons/campaign.svg"
+                  width={250}
+                  height={250}
+                />
+              </Link>
+            </StyledCard>
+          </RowDiv>
+          <RowDiv>
+            <StyledCard>
+              <StyledHeader>Coupons</StyledHeader>
+              <Link href="/coupons">
+                <Image
+                  src="/Dashboard_icons/coupon.svg"
+                  width={250}
+                  height={250}
+                />
+              </Link>
+            </StyledCard>
+          </RowDiv>
+        </ColumnDiv>
+        <ColumnDiv style={{ margin: "auto" }}>
+          <RowDiv>
+            <StyledCard>
+              <StyledHeader>Customers</StyledHeader>
+              <Link href="/customers">
+                <Image
+                  src="/Dashboard_icons/customer.svg"
+                  width={250}
+                  height={250}
+                />
+              </Link>
+            </StyledCard>
+          </RowDiv>
+          <RowDiv>
+            <StyledCard>
+              <StyledHeader>Keywords</StyledHeader>
+              <Link href="/keywords">
+                <Image
+                  src="/Dashboard_icons/keyword.svg"
+                  width={250}
+                  height={250}
+                />
+              </Link>
+            </StyledCard>
+          </RowDiv>
+        </ColumnDiv>
+      </ContainerDiv>
+    </>
+  );
+};
+
+export default Dashboard;
