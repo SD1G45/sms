191ad04e2e503d629bf0126c0ff09925f12af0b5
Alex Oh <oalex@knights.ucf.edu>
2022-02-04 14:07:49 -0500

    Keyword FAQ

--- pages/keywords/faq/FAQ.tsx
+++ pages/keywords/faq/FAQ.tsx
@@ -13,19 +13,21 @@ const FAQ = () => {
 
   return (
     <ContainerDiv>
-      <SideNav items={list} routes={routes} heading={"FAQ Keywords"} />
+      <SideNav items={list} routes={routes} heading={"Keywords"} />
       <FlexContainer>
         <StyledAccordion
           title="How to create a keyword"
-          content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+          content="Click <b><q>Create New</q></b> on the menu under the Keywords. Set your keyword title, auto response text, description, and coupons. If you wish to continue, press the blue <b><q>Create Keyword</q></b> button"
         />
         <StyledAccordion
-          title="How to create a keyword"
-          content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-        />
-        <StyledAccordion
-          title="How to create a keyword"
-          content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+          title="How to check analytics"
+          content="Click <b><q>Analytics</q></b> on the menu under the Keywords. You will be able to see your anaylitics by keywords you've created. 
+          <b><q>Customers Onboarded</q></b> shows how many customers are signed up from using the specific keyword
+          <b><q>Message success rate</q></b> shows what percentage of message sent was succesffuly delivered to a customer.
+          <b><q>Coupons opened</q></b> shows how many coupons are opened from those you have created. 
+          <b><q>Coupons redeemed</q></b> shows how many coupons are redeemed from those you have created. 
+          <b><q>Open %</q></b> is calculated by (# of coupons opened / # of coupons created). 
+          <b><q>Redeem %</q></b> is calculated by (# of coupons redeemed / # of coupons created)"
         />
       </FlexContainer>
     </ContainerDiv>
