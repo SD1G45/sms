969818dd2139ae3d489704bdb93ee768d3aff5a0
Zach Mayers <zachmayers@MacBook-Pro-2.attlocal.net>
2022-02-04 21:37:09 -0500

    Draft of accordion menu

--- components/Accordion/styles.ts
+++ components/Accordion/styles.ts
@@ -1,58 +1,62 @@
 import styled from "styled-components";
 import Button from "../../components/Button";
-import Chevron from "../../components/Accordion/Chevron";
+import { ChevronProps } from "./types";
 
-export const StyledChevron = styled(Chevron)`{
-	border-style: solid;
-	border-width: 0.25em 0.25em 0 0;
-	content: '';
-	display: inline-block;
-	height: 0.45em;
-	left: 0.15em;
-	position: relative;
-	top: 0.15em;
-
-	vertical-align: top;
-	width: 0.45em;
-  transform: ${(props) => (props.rotated ? "rotate(90deg)" : "rotate(0deg)")};
+export const Accordion = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
 `;
 
-export const AccordionSection = styled.div`
-  display: flex;
-  flex-direction: column;
+export const ListItem = styled.li`
+  border: 1px solid #ccc;
+
+  &: not(: first-of-type) {
+    border-top: 0;
+  }
+  padding: 10px;
+  background-color: rgba(72, 129, 240, 0.6);
 `;
 
-export const StyledButton = styled(Button)`
-  color: #444;
-  cursor: pointer;
-  padding: 18px;
-  // display: flex;
-  align-items: center;
-  border: none;
-  border-radius: 0%;
-  outline: none;
-  border: 2px solid white;
-  margin-left: auto;
-
-  transition: background-color 0.6s ease;
-`;
-export const ButtonDiv = styled.div`
-  align-items: center;
-  max-width: 500px;
-  display: flex;
+export const Title = styled.h2`
+  width: 100%;
+  margin: 100;
 `;
 
-export const StyledTitle = styled.p`
-  color: white;
-  text-align: left;
+export const Chevron = styled.button<ChevronProps>`
+  & {
+    content: "";
+    flex-shrink: 0;
+    width: 20px;
+    height: 18px;
+    margin-left: 95%;
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23212529'%3e%3cpath fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/%3e%3c/svg%3e");
+    background-repeat: no-repeat;
+    background-size: 100%;
+    transition: transform 0.2s ease-in-out;
+  }
+
+  &:after {
+    content: "";
+    flex-shrink: 0;
+    width: 18px;
+    height: 18px;
+    margin-left: auto;
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23212529'%3e%3cpath fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/%3e%3c/svg%3e");
+    background-repeat: no-repeat;
+    background-size: 100%;
+    transition: transform 0.2s ease-in-out;
+  }
+  transform: ${(props) => (props.active ? "rotate(180deg)" : "")};
 `;
 
-export const AccordionContent = styled.div`
-  background-color: white;
-  overflow: auto;
-  transition: max-height 0.6s ease;
+export const ItemContainer = styled.div`
+  transition: height 0.2s ease-in-out;
+  overflow: hidden;
+  // background-color: rgba(72, 240, 229, 0.5);
 `;
 
-export const AccordionText = styled.div`
-  padding: 18px;
+export const Content = styled.div`
+  border-top: 1px solid #cccccc;
+  padding: 15px 20px;
 `;
--- pages/component-testing/index.tsx
+++ pages/component-testing/index.tsx
@@ -9,18 +9,60 @@ import {
   IPhonePreview,
 } from "../../page-styles/component-testing/styles";
 
+import AccordionMenu from "../../components/Accordion/AccordionMenu";
+
 const Test = () => {
   const [value, setValue] = useState(9);
+  const accordionItems = [
+    {
+      title: "Item 1",
+      content: (
+        <div>
+          <strong>Body Header</strong> Body text <u>underline tag</u>,{" "}
+          <i>italic</i>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: "Item 2",
+      content: (
+        <div>
+          <strong>Body Header</strong> Body text <u>underline tag</u>,{" "}
+          <i>italic</i>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: "Item 3",
+      content: (
+        <div>
+          <strong>Body Header</strong> Body text <u>underline tag</u>,{" "}
+          <i>italic</i>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+      ),
+    },
+  ];
+
   return (
     <Page>
       <StyledCard>
-        <Slider
-          value={value}
-          onChange={(event: any) => setValue(event.target.value)}
-        />
+        <AccordionMenu items={accordionItems} />
       </StyledCard>
-
-      <IPhonePreview></IPhonePreview>
     </Page>
   );
 };
