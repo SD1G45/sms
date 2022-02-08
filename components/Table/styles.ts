027e1572daba42b9f9ceae564c5e40adb5c4dcd6
Brennen Fountain <brennenfountain.dev@gmail.com>
2022-02-08 18:22:35 -0500

    Centered tables

--- components/Table/styles.ts
+++ components/Table/styles.ts
@@ -1,73 +1,69 @@
 import styled from "styled-components";
 
 export const DivContainer = styled.div`
-    padding-left: 50px;
-    width: 100%;
+  padding-left: 50px;
+  width: 100%;
 `;
 
 export const DataTable = styled.table`
-    padding-top: 30px;
-    border-spacing: 0px 0px;
-    borderCollapse: collapse; 
+  padding-top: 30px;
+  border-spacing: 0px 0px;
+  width: 100%;
 `;
 
-export const TableHeader = styled.thead`
-    
-`;
+export const TableHeader = styled.thead``;
 
-export const TableBody = styled.tbody`
-
-`;
+export const TableBody = styled.tbody``;
 
 export const Pagination = styled.div`
-    float: right;
-    padding-top: 5px;
+  float: right;
+  padding-top: 5px;
 `;
 
-export const PagePointer = styled.div `
-    cursor: pointer;
-    display: inline;
-    font-weight: bold;
-    padding-left: 10px;
+export const PagePointer = styled.div`
+  cursor: pointer;
+  display: inline;
+  font-weight: bold;
+  padding-left: 10px;
 `;
 
 export const Header = styled.th`
-    border-bottom: 2.75px solid #C4C4C4;
-    padding-bottom: 15px;
-    padding-left: 58px;
-    padding-top: 15px;
-    font-size: 14px;
+  border-bottom: 2.75px solid #c4c4c4;
+  padding-bottom: 15px;
+  padding-left: 58px;
+  padding-top: 15px;
+  font-size: 14px;
 `;
 
 export const HeaderLong = styled.th`
-    border-bottom: 2.75px solid #C4C4C4;
-    padding-bottom: 15px;
-    padding-right: 100px;
-    padding-top: 15px;
-    font-size: 14px;
-    text-align: left;
+  border-bottom: 2.75px solid #c4c4c4;
+  padding-bottom: 15px;
+  padding-right: 100px;
+  padding-top: 15px;
+  font-size: 14px;
+  text-align: left;
 `;
 
 export const Data = styled.td`
-    border-bottom: 2.75px solid #C4C4C4;
-    padding-bottom: 15px;
-    padding-left: 60px;
-    padding-top: 15px;
-    font-size: 14px;
-    font-weight: 500;
+  border-bottom: 2.75px solid #c4c4c4;
+  padding-bottom: 15px;
+  padding-left: 60px;
+  padding-top: 15px;
+  font-size: 14px;
+  font-weight: 500;
 `;
 
 export const DataEmpty = styled.td`
-    border-bottom: 2.75px solid #C4C4C4;
-    padding-bottom: 45px;
-    padding-right: 60px;
-    font-size: 14px;
-    font-weight: 500;
+  border-bottom: 2.75px solid #c4c4c4;
+  padding-bottom: 45px;
+  padding-right: 60px;
+  font-size: 14px;
+  font-weight: 500;
 `;
 
 export const DataLong = styled(Data)`
-    padding-left: 0px;
-    padding-right: 40px;
-    font-weight: 500;
-    text-align: left;
+  padding-left: 0px;
+  padding-right: 40px;
+  font-weight: 500;
+  text-align: left;
 `;
