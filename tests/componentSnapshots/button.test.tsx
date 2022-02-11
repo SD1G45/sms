f88f78e4f7059a11f733f4a9c43f20c3365bfeb3
dabaquita <denielle.abaquita@gmail.com>
2022-02-11 11:46:19 -0500

    Added disabled and loading button snapshot tests

--- tests/componentSnapshots/button.test.tsx
+++ tests/componentSnapshots/button.test.tsx
@@ -7,3 +7,13 @@ test('Button renders correctly', () => {
   const tree = renderer.create(<Button/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test('Button renders as disabled', () => {
+  const tree = renderer.create(<Button disabled={true}/>).toJSON();
+  expect(tree).toMatchSnapshot();
+}); 
+
+test('Button renders as loading', () => {
+  const tree = renderer.create(<Button loading={true}/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
