de60e9a2347896f6a5317a39b3120b5d646847eb
dabaquita <denielle.abaquita@gmail.com>
2022-02-16 11:47:01 -0500

    Added test to move to register page

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -9,22 +9,48 @@ const sleep = async (ms: number) => await new Promise((res) => setTimeout(res, m
 
 beforeAll(async () => {
   browser = await puppeteer.launch({
-    headless: false
+    headless: true,
+    args: [
+      '--window-size=1920,1080',
+      '--start-maximized',
+    ],
+    defaultViewport: null
   });
   page = await browser.newPage();
 
   await page.goto(rootUrl);
 }, 30_000);
 
-test("Correctly on login page at start", async () => {
-  await sleep(1_000);
+describe("Register process", () => {
+  it("Correctly on login page at start", async () => {
+    await sleep(1_000);
+  
+    if (!page) {
+      throw new Error("Error while loading App page");
+    }
+  
+    await expect(page).toMatch('Login')
+  });
 
-  if (!page) {
-    throw new Error("Error while loading App page");
-  }
+  it("Moves to register page on clicking link", async () => {
+    if (!page) {
+      throw new Error("Error while loading App page");
+    }
 
-  await expect(page).toMatch('Login')
-});
+    const linkHandlers = await page.$x("//label[contains(., 'New to us? Create an Account')]");
+
+    if (linkHandlers.length > 0) {
+      await Promise.all([
+        linkHandlers[0].click(),
+        page.waitForNavigation()
+      ]);
+    } else {
+      throw new Error("Link not found");
+    }
+    await expect(page).toMatch('register');
+  });
+})
+
 
 afterAll(async () => {
   await browser?.close();
