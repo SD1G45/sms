9589b57561581f81850b093acd60d97bd3b2d5aa
Alex Oh <oalex@knights.ucf.edu>
2022-02-28 13:33:11 -0500

    General Settings Page

--- page-styles/settings/styles.ts
+++ page-styles/settings/styles.ts
@@ -1,18 +1,42 @@
 import styled from "styled-components";
+import Button from "../../components/Button";
 
 export const GridContainer = styled.div`
   display: flex;
+  align-items: stretch;
 `;
 
 export const Box = styled.div`
-  border: 1px solid #bec8d2;
-  padding: 100px;
-  margin: auto;
-  margin-top: 10px;
-  text-align: left;
+  border: 2px solid black;
+  padding: 15px;
+  margin: 20px;
+  width: 350px;
+  height: 250px;
 `;
 
 export const BoxHeading = styled.h2`
-  font-size: 1.4rem;
-  margin-bottom: 25px;
+  font-size: 1rem;
+`;
+export const CardHeading = styled.h1`
+  font-size: 1.5rem;
+  margin-left: 20px;
+  // text-align: left;
+`;
+
+export const Description = styled.p`
+  font-size: 0.9rem;
+  padding-top: 1px;
+  padding-bottom: 12px;
+  color: gray;
+`;
+
+export const StyledButton = styled(Button)`
+  display: inline-flex;
+  align-items: center;
+  font-size: 13px;
+  color: #4881f0;
+  background-color: white;
+  text-align: left;
+  margin-left: -15px;
+  height: 10px;
 `;
--- pages/settings/Settings.tsx
+++ pages/settings/Settings.tsx
@@ -5,26 +5,65 @@ import {
   Box,
   BoxHeading,
   GridContainer,
+  CardHeading,
+  Description,
+  StyledButton,
 } from "../../page-styles/settings/styles";
+import {
+  FcQuestions,
+  FcDataEncryption,
+  FcMoneyTransfer,
+  FcConferenceCall,
+  FcSupport,
+} from "react-icons/fc";
 
 const Settings = () => {
   return (
     <SingleCardPage>
       <StyledCard>
+        <CardHeading>
+          <FcSupport /> Settings
+        </CardHeading>
         <GridContainer>
           <Box>
-            <BoxHeading>Profile</BoxHeading>
+            <BoxHeading>
+              <FcDataEncryption />
+              Profile
+            </BoxHeading>
+            <Description>Change your basic information </Description>
+            <StyledButton>Name and email</StyledButton>
+            <StyledButton>Phone number</StyledButton>
+            <StyledButton>Password reset</StyledButton>
           </Box>
           <Box>
-            <BoxHeading>Billing</BoxHeading>
+            <BoxHeading>
+              <FcMoneyTransfer />
+              Billing
+            </BoxHeading>
+            <Description>Change your billing information </Description>
+            <StyledButton>Subscription</StyledButton>
+            <StyledButton>Card information</StyledButton>
+            <StyledButton>Payment history</StyledButton>
           </Box>
         </GridContainer>
         <GridContainer>
           <Box>
-            <BoxHeading>Business settings</BoxHeading>
+            <BoxHeading>
+              <FcConferenceCall />
+              Team
+            </BoxHeading>
+            <Description>Manage your team members </Description>
+            <StyledButton>Manage roles </StyledButton>
+            <StyledButton>Invite a team member</StyledButton>
           </Box>
           <Box>
-            <BoxHeading>Team</BoxHeading>
+            <BoxHeading>
+              <FcQuestions />
+              Help
+            </BoxHeading>
+            <Description>Contact Trism </Description>
+            <StyledButton>Email Trism </StyledButton>
+            <StyledButton>Text Trism </StyledButton>
           </Box>
         </GridContainer>
       </StyledCard>
