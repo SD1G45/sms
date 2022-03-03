bac178085298cadfa97d2790a021dd496f7e8ba0
Zach Mayers <zachmayers@MacBook-Pro-2.local>
2022-03-02 19:53:22 -0500

    Bandaid fix on logout function

--- pages/settings/profile/phone-number/PhoneNumber.tsx
+++ pages/settings/profile/phone-number/PhoneNumber.tsx
@@ -1,17 +1,36 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { StyledCard } from "../../../../components/Card/styles";
 import SingleCardPage from "../../../../components/SingleCardPage";
 import { useBusinessState } from "../../../../context/BusinessContext/BusinessContext";
-import { Heading } from "../../../../page-styles/settings/profile/styles";
-
+import newRouteWithQueries from "../../../../helpers/newRouteWithQueries";
+import {
+  Heading,
+  StyledLink,
+  PhoneNumberDiv,
+  PhoneNumberLinkDiv,
+} from "../../../../page-styles/settings/profile/styles";
+import { FcPhone } from "react-icons/fc";
 const PhoneNumber = () => {
   const businessState = useBusinessState();
-
+  const router = useRouter();
   return (
     <SingleCardPage>
       <StyledCard>
         <Heading>Phone number attached to {businessState?.name}:</Heading>
-        {businessState?.phoneNumber}
+        <PhoneNumberDiv>
+          <FcPhone />
+          {businessState?.phoneNumber}
+        </PhoneNumberDiv>
+        <PhoneNumberLinkDiv>
+          <Link href={newRouteWithQueries("/settings", router)}>
+            <StyledLink>Back to settings</StyledLink>
+          </Link>
+          <Link href={newRouteWithQueries("/settings", router)}>
+            <StyledLink>Back to settings</StyledLink>
+          </Link>
+        </PhoneNumberLinkDiv>
       </StyledCard>
     </SingleCardPage>
   );
