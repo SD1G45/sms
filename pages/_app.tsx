ce07ddac985d51d39cbb61dda4c7356d52dc129d
Zachary <rick.jsventures.com>
2022-03-04 17:25:42 -0500

    Added loading spinner on new page load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,48 @@ import { useApollo } from "../lib/apolloClient";
 import { UserProvider } from "../context/UserContext/UserContext";
 import { BusinessProvider } from "../context/BusinessContext/BusinessContext";
 import RouteGuard from "../components/RouteGuard";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import LoadingDiv from "../components/LoadingDiv";
 
+const Loading = () => {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url: any) => url && setLoading(true);
+    const handleComplete = (url: any) => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  });
+
+  return (
+    <>
+      {loading ? (
+        <div>
+          <LoadingDiv />
+        </div>
+      ) : (
+        <>d</>
+      )}
+    </>
+  );
+};
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
     <ApolloProvider client={apolloClient}>
+      <Loading />
       <UserProvider>
         <BusinessProvider>
           <GlobalStyles />
--- pages/component-testing/index.tsx
+++ pages/component-testing/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../../components/Card";
 import { StyledCard } from "../../components/Card/styles";
 import Slider from "../../components/Slider";
@@ -7,12 +7,22 @@ import {
   Page,
   TimerBox,
   IPhonePreview,
+  pageLoadingDiv,
+  StyledLoadCard,
 } from "../../page-styles/component-testing/styles";
-
+import LoadingDiv from "../../components/LoadingDiv";
 import AccordionMenu from "../../components/Accordion/AccordionMenu";
-
+import Spinner from "../../components/Spinner";
+import Button from "../../components/Button";
+import { LinkDiv, StyledLink } from "../../page-styles/login/styles";
+import Link from "next/link";
+import newRouteWithQueries from "../../helpers/newRouteWithQueries";
+import { useRouter } from "next/router";
 const Test = () => {
   const [value, setValue] = useState(9);
+
+  const [pageLoading, setPageLoading] = useState(false);
+
   const accordionItems = [
     {
       title: "Item 1",
@@ -58,12 +68,25 @@ const Test = () => {
     },
   ];
 
+  const router = useRouter();
+  const pageLoadingDisplay = () => {
+    return <StyledLoadCard>Hello</StyledLoadCard>;
+  };
+
   return (
-    <Page>
-      <StyledCard>
-        <AccordionMenu items={accordionItems} />
-      </StyledCard>
-    </Page>
+    <>
+      <Page>
+        {pageLoading && (
+          <LoadingDiv>
+            <Spinner size={50} sizeUnit="px" color="#9999" />
+          </LoadingDiv>
+        )}
+        <StyledCard>
+          <Button onClick={() => setPageLoading(true)}>Load</Button>
+          {/* <AccordionMenu items={accordionItems} /> */}
+        </StyledCard>
+      </Page>
+    </>
   );
 };
 
