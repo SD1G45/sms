e185e7ac8bca158d5d3f31e9f72ebcf30226fc4e
dabaquita <denielle.abaquita@gmail.com>
2022-03-08 12:31:53 -0500

    Refactored cleanDatabase() to include relations with user

--- tests/utils/e2eHelpers.ts
+++ tests/utils/e2eHelpers.ts
@@ -1,3 +1,4 @@
+import { BusinessName } from "../../components/Navbar/styles";
 import { prisma } from "../../graphql/context/prisma";
 
 export const testUser = {
@@ -5,18 +6,48 @@ export const testUser = {
   lastName: "LastName",
   email: "sample@email.com",
   password: "password",
-  badPassowrd: "badPassword",
+  badPassowrd: "badPassword"
 };
 
+export const testBusiness = {
+  name: "TestBusiness",
+}
+
 export const cleanDatabase = async ( errorMessage: string ) => {
-  // Clean up database in case
-  try {
-    await prisma.user.delete({
-      where: {
-        email: testUser.email
-      }
-    });
-  } catch (error) {
-    console.log(errorMessage);
-  }
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          email: testUser.email,
+        }
+      })
+
+      // Delete all Business_Users
+      await prisma.business_User.deleteMany({
+        where: {
+          userId: user?.id,
+        }
+      })
+  
+      // Delete user
+      await prisma.user.delete({
+        where: {
+          id: user?.id
+        }
+      });
+
+      // Delete business
+      const business = await prisma.business.findFirst({
+        where: {
+          name: testBusiness.name,
+        }
+      })
+      await prisma.business.delete({
+        where: {
+          id: business?.id,
+        }
+      })
+      
+    } catch (error) {
+      console.log(errorMessage);
+    }
 };
