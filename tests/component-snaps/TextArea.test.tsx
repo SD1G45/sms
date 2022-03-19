ed37d419d93b0ec0543f9646480b6e583fa2914e
dabaquita <denielle.abaquita@gmail.com>
2022-03-19 13:44:25 -0400

    Added TextArea snapshots

---
+++ tests/component-snaps/TextArea.test.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+import Slider from "../../components/Slider";
+import Theme from "../../components/styles/Theme";
+import TextArea from "../../components/TextArea";
+
+const steps = ["Step 1", "Step 2", "Step 3"];
+
+describe("<TextArea/>", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(
+      <Theme>
+        <TextArea
+          id="test"
+          label="Test"
+          value="test"
+        />
+      </Theme>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders correctly with error", () => {
+    const tree = renderer.create(
+      <Theme>
+        <TextArea
+          id="test"
+          label="Test"
+          value="test"
+          error={true}
+          errorMessage="Test Error"
+        />
+      </Theme>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
