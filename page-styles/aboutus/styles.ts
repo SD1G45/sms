41deafe9553204e217dfd9f7fd324a37f3f9f552
Alex Oh <oalex@knights.ucf.edu>
2022-04-06 16:59:09 -0400

    Trism AboutUs Page

---
+++ page-styles/aboutus/styles.ts
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+export const HeaderDiv = styled.div`
+  width: 100%;
+  padding-top: 170px;
+  padding-left: 200px;
+  height: 75%;
+  background-color: #4881f0;
+`;
+export const ImageDiv = styled.div`
+  margin: auto;
+  display: flex;
+  align-items: center;
+  width: 50%;
+  height: 75%;
+`;
+export const SecondHeaderDiv = styled.div`
+  width: 80%;
+  padding-top: 170px;
+  padding-left: 200px;
+  height: 75%;
+`;
+
+export const ColumnDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const RowDiv = styled.div`
+  // display: flex;
+`;
+export const StyledHeader = styled.h1`
+  overflow: hidden;
+  // white-space: nowrap;
+  font-size: 60px;
+  color: white;
+`;
+export const SecondHeader = styled.h1`
+  // margin-top: 300px;
+  overflow: hidden;
+  // white-space: nowrap;
+  font-size: 60px;
+  margin-bottom: 15px;
+  // color: white;
+`;
+export const StyledBody = styled.h1`
+  font-weight: normal;
+  overflow: hidden;
+  white-space: nowrap;
+  color: white;
+  margin-top: 5px;
+  font-size: 20px;
+`;
+export const SecondBody = styled.h1`
+  font-weight: normal;
+  // overflow: hidden;
+  // white-space: nowrap;
+  line-height: 2rem;
+  // color: white;
+  margin-top: 5px;
+  font-size: 20px;
+  margin-bottom: 20px;
+`;
+export const ContainerDiv = styled.div`
+  display: flex;
+  //   padding: 80px 80px;
+  // background-color: #4881f0;
+  // background-color: #4881f0;
+  width: 100%;
+  margin-top: -1px;
+  height: 80%;
+`;
+
+export const LineDiv = styled.div`
+  margin-top: 10px;
+  height: 5px;
+  background-color: red;
+  width: 320px;
+  margin-bottom: 10px;
+`;
+
+// export const FlexContainer = styled.div`
+//   display: flex;
+//   padding: 50px 80px;
+//   width: 100%;
+//   /* background-color: green; */
+// `;
